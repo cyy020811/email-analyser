e79e0dfe18cef5c7e2aeae6f8c88d3f9d92b286a
cyy020811 <jimyang811@gmail.com>
2025-08-02 21:12:41 +1000

    feat: complete interaction end point

--- backend/analyser/src/main/java/com/email/analyser/controller/InteractionController.java
+++ backend/analyser/src/main/java/com/email/analyser/controller/InteractionController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.email.analyser.dto.InteractionDto;
 import com.email.analyser.service.InteractionService;
-
 import lombok.AllArgsConstructor;
 
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @RestController
 @AllArgsConstructor
@@ -21,8 +21,10 @@ public class InteractionController {
     private final InteractionService interactionService;
 
     @GetMapping("/{userId}")
-    public Iterable<InteractionDto> getInteractions(@PathVariable UUID usreId) {
-        return interactionService.getUserInteractions(usreId);
-    }
+    public Iterable<InteractionDto> getInteractions(@PathVariable UUID userId,
+            @RequestHeader("Authorization") String authoriazation) {
 
+        String token = authoriazation.replace("Bearer ", "").trim();
+        return interactionService.getUserInteractions(userId, token);
+    }
 }
